b23b1f97b7afb9b7d202a668f3a0b79192a415d2
Erwing Choquerive Quispe <choquerive.erwing123@gmail.com>
2018-04-27 08:06:44 +0200

    creacion y añadicion a .gitignore

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+/**
+*Creando un texto de salida con java
+*texto de salida sera Hola Mundo !!!
+*/
+
+public class HolaMundo {
+    public static void main (String[] args)
+   {
+     System.out.println(" Hola Mundo !!!"); 
+   }
+}
